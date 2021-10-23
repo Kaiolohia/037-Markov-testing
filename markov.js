6d070f2d9e172ab6adc1a1d28240034ea7301be0
Kai "Trux <59106905+TruxSux@users.noreply.github.com>
2021-10-22 22:01:12 -1000

    Finished markov.js

--- markov.js
+++ markov.js
@@ -1,5 +1,8 @@
 /** Textual markov chain generator */
 
+function randint(max) {
+  return Math.floor(Math.random() * max);
+}
 
 class MarkovMachine {
 
@@ -8,7 +11,7 @@ class MarkovMachine {
   constructor(text) {
     let words = text.split(/[ \r\n]+/);
     this.words = words.filter(c => c !== "");
-    this.makeChains();
+    this.chain = this.makeChains(this.words);
   }
 
   /** set markov chains:
@@ -16,14 +19,46 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
-    // TODO
+  makeChains(words) {
+    let chain = {}
+    for (let i=0;i<words.length;i++) {
+      if (chain.hasOwnProperty(words[i])) {
+        chain[words[i]].push(words[i+1])
+      }
+      else {
+        chain[words[i]] = [words[i+1]]
+      }
+    }
+    return chain
   }
-
 
   /** return random text from chains */
 
-  makeText(numWords = 100) {
-    // TODO
+  makeText(startingWord, numWords = 100) {
+    let i = 0
+    let phrase = []
+    let keys = Object.keys(this.chain);
+    let currentKey = startingWord
+    phrase.push(startingWord)
+    while(true) {
+      if (i==numWords) {
+        break;
+      }
+      let word = this.chain[currentKey]
+
+        word = word[randint(word.length)]
+        
+      if (!word) {
+        break;
+      }
+      phrase.push(word)
+      currentKey = word
+      i++
+    }
+    return phrase
   }
 }
+
+let markovMachine = new MarkovMachine("the cat in the hat is in the hat")
+console.log(markovMachine.chain)
+markovMachine.makeText("in",6)
